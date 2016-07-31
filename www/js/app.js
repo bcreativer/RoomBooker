29e5d9b3e184fdb506b4c0e06919ec7dca264176
Beston Nounrasi <bcreativer@gmail.com>
2016-07-31 22:14:46 +1000

    Filtering Added

--- www/js/app.js
+++ www/js/app.js
@@ -22,9 +22,8 @@ angular.module('starter', ['ionic'])
     }
   });
 })
-
 .controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
-    $scope.bookings = data;
+    $scope.bookings = data.bookings;
   });
 }]);
